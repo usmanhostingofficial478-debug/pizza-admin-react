65cd305bcf8f1dbc6aa87f1cf9ee792b6b37fbe9
usmanhostingofficial478-debug <usmanhostingofficial478@users.noreply.github.com>
2026-05-03 19:26:49 +0500

    FixVercelOutput

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
